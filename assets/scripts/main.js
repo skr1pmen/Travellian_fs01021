43ff7e199de82f54c27bc799dd7f56e41266bb8a
skr1pmen <kiberkot.v.roymenge@gmail.com>
2026-09-19 20:45:24 +0400

    update 19.09.2026

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,3 +9,14 @@ left_btn.addEventListener('click', () => {
 right_btn.addEventListener('click', () => {
     slider.scrollBy({ left: 497, behavior: "smooth" });
 });
+
+
+
+const f_slider = document.querySelector("#f_slider");
+
+const left_slide = () => {
+    f_slider.scrollBy({ left: -663, behavior: "smooth" });
+}
+function right_slide() {
+    f_slider.scrollBy({ left: 663, behavior: "smooth" });
+}
